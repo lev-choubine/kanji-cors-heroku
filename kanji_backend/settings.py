81c9c9925c7300a9769da3988a53f5f7d4f2d2b9
Lev Michael <lev.choubine@gmail.com>
2021-02-10 19:10:41 -0500

    reset collect static

--- kanji_backend/settings.py
+++ kanji_backend/settings.py
@@ -28,7 +28,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
